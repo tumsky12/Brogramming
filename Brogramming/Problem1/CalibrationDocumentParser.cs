51fe3e38605d109bc1f73c2ece06f21b93c2f78d
tumsky12 <atumtum12@gmail.com>
2023-12-16 18:29:11 +0000

    First part of first problem solved correctly

---
+++ Brogramming/Problem1/CalibrationDocumentParser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brogramming.Problem1;
+
+public class CalibrationDocumentParser
+{
+
+    public Dictionary<string, int> NumberMapping { get; set; } = new Dictionary<string, int>()
+    {
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+        {"zero", 0},
+    };
+
+    public int GetTwoDigitNumber(string line)
+    {
+        string? firstDigit = null;
+        string lastDigit = null;
+
+        var characterArray = line.ToCharArray();
+        // ["1", "a", "b", "c" , "2"]
+
+        foreach (var c in characterArray)
+        {
+            var canParse = int.TryParse(c.ToString(), out int digit);
+
+            if (canParse)
+            {
+                if (firstDigit == null)
+                {
+                    firstDigit = c.ToString();
+                }
+
+                lastDigit = c.ToString();
+            }
+        }
+
+        var twoDigitNumber = firstDigit + lastDigit;
+
+        return int.Parse(twoDigitNumber);
+    }
+
+    public int AddListOfDigitsTogether(List<int> digits)
+    {
+        return digits.Sum();
+    }
+
+    public int ParseDocument(string calibrationDocument)
+    {
+        var listOfDigits = new List<int>();
+
+        var arrayOfLines = calibrationDocument.Split('\n');
+        foreach (var line in arrayOfLines)
+        {
+            var digit = GetTwoDigitNumber(line);
+            listOfDigits.Add(digit);
+        }
+
+        var result = AddListOfDigitsTogether(listOfDigits);
+
+        return result;
+    }
+
+}
---
+++ BrogrammingTests/Problem1/CalibrationDocumentParserTests.cs
@@ -0,0 +1,67 @@
+using Brogramming.Problem1;
+
+namespace BrogrammingTests.Problem1;
+
+public class Tests
+{
+    //[SetUp]
+    //public void Setup()
+    //{
+    //}
+
+    [TestCase("1abc2", 12)]
+    [TestCase("treb7uchet", 77)]
+    [TestCase("two1nine", 29)]
+    [TestCase("eightwothree", 83)]
+    [TestCase("abcone2threexyz", 13)]
+    [TestCase("xtwone3four", 24)]
+    [TestCase("4nineeightseven2", 42)]
+    [TestCase("zoneight234", 14)]
+    [TestCase("7pqrstsixteen", 76)]
+    public void GetTwoDigitNumber_ReturnsCorrectTwoDigitNumber(string testLine, int expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var parser = new CalibrationDocumentParser();
+        var actualResult = parser.GetTwoDigitNumber(testLine);
+
+        // Assert
+        Assert.That(expectedResult, Is.EqualTo(actualResult));
+    }
+    
+    [Test]
+    public void AddListOfDigitsTogether_CorrectlyAddsAllDigitsTogether()
+    {
+        // Arrange
+        var testDigits = new List<int>{12, 38, 15, 77};
+        var expectedResult = 142;
+
+        // Act
+        var parser = new CalibrationDocumentParser();
+        var actualResult = parser.AddListOfDigitsTogether(testDigits);
+
+        // Assert
+        Assert.That(expectedResult, Is.EqualTo(actualResult));
+    }
+
+    [Test]
+    public void ParseDocument_CorrectlyParsesDocument()
+    {
+        // Arrange
+        var document = @"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        var expectedResult = 142;
+
+        // Act
+        var parser = new CalibrationDocumentParser();
+        var actualResult = parser.ParseDocument(document);
+
+        // Assert
+        Assert.That(expectedResult, Is.EqualTo(actualResult));
+    }
+
+
+}
